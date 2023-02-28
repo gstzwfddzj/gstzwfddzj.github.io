430283ebe65a781246cf881bd113f393c2823fe6
Xsr <10179649+gstzwfddzj@user.noreply.gitee.com>
2023-02-28 17:01:48 +0800

    修改小功能

--- time/style.js
+++ time/style.js
@@ -1,10 +1,14 @@
 let front = document.querySelector("#front");
 let start = document.getElementById("start");
 let time = document.querySelector(".time");
+let before = document.querySelector(".before");
 let day = document.querySelector("#day");
+let day2 = document.querySelector("#day2");
 let [, p1, p2, p3] = document.querySelector(".result").children;
-let { hour, min } = createTime();
+let { hour, min } = createTime(time);
+let { hour: hour2, min: min2 } = createTime(before);
 
+// 获取存储时间
 let storage = localStorage.getItem("daojishitime");
 if (storage) {
   let obj = JSON.parse(storage);
@@ -13,13 +17,22 @@ if (storage) {
   day.value = obj.day1;
   hour.value = obj.hour1;
   min.value = obj.min1;
+  day2.value = obj?.day3 || 0;
+  hour2.value = obj?.hour3 || 0;
+  min2.value = obj?.min3 || 0;
 }
 
 hour.onchange = change;
 min.onchange = change;
+
+hour2.onchange = change;
+min2.onchange = change;
+day2.onchange = change;
+
 start.onchange = change;
 front.onchange = change;
 day.onchange = change;
+
 
 setInterval(() => {
   mathTime();
@@ -32,36 +45,30 @@ function mathTime() {
   let day1 = day.value;
   let hour1 = hour.value;
   let min1 = min.value;
+  let day3 = day2.value;
+  let hour3 = hour2.value;
+  let min3 = min2.value;
   if (start1 && front1 && day1 && hour1 && min1) {
+    // 目标时间
     let mubiao = new Date(start1);
-    let daoda = new Date(front1);
+    // 到达时间 提前时间
+    // let daoda = new Date(front1);
+    let daoda = mubiao - 1000 * (day3 * 24 * 3600 + hour3 * 60 * 60 + min3 * 60)
+// 定义 出行时间变量 ， 还有多少分钟 ， 出发时间
     let chuxing,
       chaju = mubiao - daoda,
       chufa;
+      // 到达时间 - 路程时间  =  出发时间
     let shijianchuo = daoda - 1000 * (day1 * 24 * 3600 + hour1 * 60 * 60 + min1 * 60);
+    // 化为 日整数 来计算
     chuxing = new Date(new Date(shijianchuo).toLocaleDateString()) - new Date(new Date().toLocaleDateString());
-    chufa = new Date(shijianchuo) - Date.now();
-    let ri = parseInt(chuxing / 1000 / 60 / 60 / 24);
-    if (ri >= 0) {
-      if (ri == 0) {
-        chuxing = "今天 ";
-      } else if (ri == 1) {
-        chuxing = "明天 ";
-      } else if (ri == 2) {
-        chuxing = "后天 ";
-      } else if (ri == 3) {
-        chuxing = "大后天 ";
-      } else {
-        chuxing = new Date(shijianchuo).toLocaleDateString();
-      }
-    } else {
-      chuxing = new Date(shijianchuo).toLocaleDateString();
-    }
+    // 出发时间
+    chufa = shijianchuo - Date.now();
     let s1 = p1.children[1];
     let s2 = p2.children[1];
     let s3 = p3.children[1];
     let time2 = new Date(shijianchuo).toLocaleTimeString().split(":");
-    s1.innerText = chuxing + `\xa0\xa0` + time2[0] + " 点 " + time2[1] + " 分 " + time2[2] + " 秒";
+    s1.innerText = mthd(chuxing , shijianchuo) + `\xa0\xa0` + time2[0] + " 点 " + time2[1] + " 分 " + time2[2] + " 秒";
     s2.innerText = huangsuan(+chaju);
     s3.innerText = huangsuan(chufa);
   }
@@ -75,9 +82,34 @@ function change(e) {
   let day1 = day.value;
   let hour1 = hour.value;
   let min1 = min.value;
+  // let 死区 所以不能一样
+  let day3 = day2.value;
+  let hour3 = hour2.value;
+  let min3 = min2.value;
   if (start1 && front1 && day1 && hour1 && min1) {
-    localStorage.setItem("daojishitime", JSON.stringify({ start1, front1, day1, hour1, min1 }));
+    localStorage.setItem("daojishitime", JSON.stringify({ start1, front1, day1, hour1, min1, day3, hour3, min3 }));
   }
+}
+
+// 近日换算说法
+function mthd(chuxing ,shijianchuo ) {
+  let ri = parseInt(chuxing / 1000 / 60 / 60 / 24);
+  if (ri >= 0) {
+    if (ri == 0) {
+      chuxing = "今天 ";
+    } else if (ri == 1) {
+      chuxing = "明天 ";
+    } else if (ri == 2) {
+      chuxing = "后天 ";
+    } else if (ri == 3) {
+      chuxing = "大后天 ";
+    } else {
+      chuxing = new Date(shijianchuo).toLocaleDateString();
+    }
+  } else {
+    chuxing = new Date(shijianchuo).toLocaleDateString();
+  }
+  return chuxing;
 }
 
 // 换算
@@ -93,7 +125,7 @@ function huangsuan(num) {
 }
 
 // 创建时间选择
-function createTime() {
+function createTime(time) {
   let hour = document.createElement("select");
   hour.name = "时";
   let min = document.createElement("select");
